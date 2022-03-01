0042f24dc44117b0ee997210065e58907a4d4cd4
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-03-01 17:24:39 +0000

    Removed comments

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -1,11 +1,4 @@
 # This is a sample commands.py.  You can add your own commands here.
-#
-# Please refer to commands_full.py for all the default commands and a complete
-# documentation.  Do NOT add them all here, or you may end up with defunct
-# commands when upgrading ranger.
-
-# A simple command for demonstration purposes follows.
-# -----------------------------------------------------------------------------
 
 from __future__ import (absolute_import, division, print_function)
 
