ef804e5a09c7478536325e7a24fee4804a1620e3
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-03-07 20:02:45 +0000

    Added udiskie and dunst

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -3,3 +3,5 @@ xset s off &
 nitrogen --restore &
 redshift -P -O 3200 &
 picom --config ~/.config/picom/picom.conf --experimental-backends &
+udiskie  --no-appindicator &
+dunst -conf /home/jorge/.config/dunst/qtilerc &
