a553c34d05f194f4d1c15e9f44ec00c48dce95dd
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-03-07 18:04:22 +0000

    Customized 'Monadtall' and 'stack' layouts

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -89,9 +89,9 @@ for i in groups:
     )
 
 layouts = [
-    layout.MonadTall(border_focus="#39ff14", border_normal="#228b22", border_width=1, margin=6),
+    layout.MonadTall(border_focus="#ff79c6", border_normal="#990057", border_width=1, margin=6),
     layout.Max(),
-    layout.Stack(num_stacks=2),
+    layout.Stack(border_focus="#ff79c6", border_normal="#990057", border_width=1, num_stacks=2),
     # layout.Columns(border_focus=["#d75f5f", "#8f3d3d"], border_width=4, margin=8),
     # layout.Bsp(),
     # layout.Matrix(),
