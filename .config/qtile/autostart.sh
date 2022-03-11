0fc8eefc76764f5ec2ee343b4bcce81349284465
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-03-11 08:18:27 +0000

    Set picom to have transparency instead of blur background

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -2,6 +2,6 @@
 xset s off &
 nitrogen --restore &
 redshift -P -O 3200 &
-picom --config ~/.config/picom/picom.conf --experimental-backends &
+picom --config ~/.config/picom/picom.conf &
 udiskie  --no-appindicator &
 dunst -conf /home/jorge/.config/dunst/qtilerc &
