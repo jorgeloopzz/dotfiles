107861fe6e56904ca26805bd56cc23a4e9609efb
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-03-16 18:11:27 +0000

    Removed programs in autostart to put them in .xprofile

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-xset s off &
-nitrogen --restore &
-redshift -P -O 3200 &
 picom --config ~/.config/picom/picom.conf &
-udiskie  --no-appindicator &
 dunst -conf /home/jorge/.config/dunst/qtilerc &
