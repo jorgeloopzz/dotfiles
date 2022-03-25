388c7050feba40f0302bafeb3357eca70ea3ecd3
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-03-25 18:40:28 +0000

    Changed bar background color

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -132,6 +132,7 @@ screens = [
                 widget.QuickExit(),
             ],
             30,
+            background="#191724",
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
