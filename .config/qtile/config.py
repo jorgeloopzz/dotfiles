421316e8ff60547edae89a142b93e748993020d7
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-03-25 17:46:01 +0000

    Removed some descriptions

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -96,14 +96,12 @@ for i in groups:
                 [mod],
                 i.name,
                 lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
             ),
             # mod1 + shift + letter of group = switch to & move focused window to group
             Key(
                 [mod, "shift"],
                 i.name,
                 lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
             ),
             # Or, use below if you prefer not to switch to that group.
             # # mod1 + shift + letter of group = move focused window to group
