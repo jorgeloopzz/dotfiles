c4002e0baa86daf46ad4535ff77d29bb1840616e
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-03-25 18:14:14 +0000

    Set workspaces icons

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -86,29 +86,18 @@ keys = [
     Key([mod, "shift"], "e", lazy.shutdown()),
 ]
 
-groups = [Group(i) for i in "123456789"]
+groups = [Group(i) for i in [
+    "   ", "   ", "   ", "  ", "   ", "   ", "   ",
+]]
 
-for i in groups:
-    keys.extend(
-        [
-            # mod1 + letter of group = switch to group
-            Key(
-                [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-            ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-            ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
-        ]
-    )
+for i, group in enumerate(groups):
+    actual_key = str(i + 1)
+    keys.extend([
+        # Switch to workspace N
+        Key([mod], actual_key, lazy.group[group.name].toscreen()),
+        # Send window to workspace N
+        Key([mod, "shift"], actual_key, lazy.window.togroup(group.name))
+    ])
 
 layouts = [
     layout.MonadTall(border_focus="#c4a7e7", border_normal="#602ca0", border_width=1, margin=9),
