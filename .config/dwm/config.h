afdf9fb73c1e36ab2c30cc1ddb6478222a150b9d
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-08-10 19:49:31 +0100

    Removed discord ws and changed terminal icon

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "  ", "   ", "  ", "  ", "   ", "   ", " ﭮ ", "  " };
+static const char *tags[] = { "  ", "  ", "  ", "  ", "   ", "   ", "  " };
 
 static const Rule rules[] = {
 	/* xprop(1):
