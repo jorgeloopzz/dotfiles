081099aa2a8b5c7b13d96966f76fab7c90a67522
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-08-21 23:33:16 +0100

    Set battery format

--- .config/dwm/blocks.h
+++ .config/dwm/blocks.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "free -h | awk '/^Mem:/ {print $3}'",	1,		0},
+	{" ",	"free -h | awk '/^Mem:/ {print $3}'",		1,		0},
 
-	{" ", "date '+%d/%m/%Y - %R'",		60,		0},
+	{" ", "date '+%d/%m/%Y - %R'",				60,		0},
+
+	{" ", "acpi | awk '{print $4}' | sed 's/,//'",		60,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
