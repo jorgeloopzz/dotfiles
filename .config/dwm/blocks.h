ce43855cb2cd585ea06590e51ce1751f75d094b0
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-08-21 15:06:15 +0100

    Set RAM format

--- .config/dwm/blocks.h
+++ .config/dwm/blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{" ", "free -h | awk '/^Mem:/ {print $3}'",	1,		0},
 
 	{" ", "date '+%d/%m/%Y - %R'",		60,		0},
 };
