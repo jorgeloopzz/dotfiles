574ac16c31a4ef144de62302d6bdeaf34ea1480a
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-08-21 17:52:04 +0100

    Set keybindings for volume and brightness

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -62,6 +63,15 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
+/* Volume */
+static const char *mutecmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
+static const char *volupcmd[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
+static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL };
+
+/* Brightness */
+static const char *brupcmd[] = { "xbacklight", "-inc", "10", NULL };
+static const char *brdowncmd[] = { "xbacklight", "-dec", "10", NULL };
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          SHCMD("rofi -show drun") },
@@ -97,6 +107,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ 0, 			XF86XK_AudioMute, 		spawn, 		{.v = mutecmd}		},
+	{ 0,			XF86XK_AudioLowerVolume, 	spawn,		{.v = voldowncmd} 	},
+	{ 0,			XF86XK_AudioRaiseVolume, 	spawn,		{.v = volupcmd}		},
+	{ 0,			XF86XK_MonBrightnessUp, 	spawn,		{.v = brupcmd} 		},
+	{ 0,			XF86XK_MonBrightnessDown, 	spawn,		{.v = brdowncmd}	},
 };
 
 /* button definitions */
