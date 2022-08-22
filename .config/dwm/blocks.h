2390dfd2a0407b646e3534625231a693a18568e8
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-08-22 22:18:12 +0100

    Set volume format

--- .config/dwm/blocks.h
+++ .config/dwm/blocks.h
@@ -1,11 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ",	"free -h | awk '/^Mem:/ {print $3}'",		1,		0},
+	/*Icon*/				/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{" ",			"free -h | awk '/^Mem:/ {print $3}'",			1,		0},
 
-	{" ", "date '+%d/%m/%Y - %R'",				60,		0},
+	{" ", 			"date '+%d/%m/%Y - %R'",				60,		0},
 
-	{" ", "acpi | awk '{print $4}' | sed 's/,//'",		60,		0},
+	{" ", "amixer get Master | tail -n1 | awk '{print $5}' | cut -c 2- | cut -c -3",		1,		0},
+
+	{" ", 			"acpi | awk '{print $4}' | sed 's/,//'",		60,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
