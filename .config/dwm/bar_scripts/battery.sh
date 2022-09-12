13d29ee0f76f25d3a7ff8454e4f083e7891cbb05
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-09-12 20:17:01 +0100

    Fixed 'if-elif' and added a charging state

--- .config/dwm/bar_scripts/battery.sh
+++ .config/dwm/bar_scripts/battery.sh
@@ -2,12 +2,16 @@
 
 battery=`acpi | awk '{print $4}' | sed 's/,//'`
 
-if [[ $battery < 100 ]]; then
-	echo -n "  $battery"
-elif [[ $battery < 75 ]]; then
-	echo -n "  $battery"
-elif [[ $battery < 50 ]]; then
-	echo -n "  $battery"
+state=`acpi | awk '{print $3}' | sed 's/,//'`
+
+if [[ $state == "Charging" ]]; then
+	echo -n " $battery"
 elif [[ $battery < 25 ]]; then
 	echo -n "  $battery"
+elif [[ $battery < 50 ]]; then
+	echo -n "  $battery"
+elif [[ $battery < 75 ]]; then
+	echo -n "  $battery"
+elif [[ $battery < 100 ]]; then
+	echo -n "  $battery"
 fi
