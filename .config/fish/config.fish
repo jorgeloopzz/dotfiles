fe18d9373e2e86c50b8339896ea956e845a9deab
Jorge López <80071604+jorgeloopzz@users.noreply.github.com>
2023-08-17 13:52:08 +0100

    Removed sudo for remove alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,7 @@ if status is-interactive
 	alias l="exa -la --icons"
 	alias cl="clear"
 	alias fonts="fc-list : family | grep Nerd | sort | awk '{print $1 $2 $3}' | uniq"
-	alias remove="sudo shred -zvu"
+	alias remove="shred -zvu"
 
 	# Dotfiles aliases
 	alias dotfiles="/usr/bin/env git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
