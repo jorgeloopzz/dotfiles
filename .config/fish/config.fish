78ac2eaf5fb785d0ab9928046359cdec4bcb8d2b
jorgeloopzz <jorgeloopzz@protonmail.com>
2023-08-26 02:11:49 +0100

    Made an alias to push dotfiles commits

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,11 +12,11 @@ if status is-interactive
 	alias cl="clear"
 	alias fonts="fc-list : family | grep Nerd | sort | awk '{print $1 $2 $3}' | uniq"
 	alias remove="sudo shred -zvu"
-
 	# Dotfiles aliases
 	alias dotfiles="/usr/bin/env git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
 	alias da="dotfiles add"
 	alias dc="dotfiles commit -m"
+	alias dp="dotfiles push origin master"
 
 	## INIT SCRIPT ##
 	colorscript random
